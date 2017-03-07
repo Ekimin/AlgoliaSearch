0144ac432ab7cead478bc81a0ccfd5773f4e8995
Ekimin <heyiminwork@gmail.com>
2017-03-07 15:27:10 +0800

    save

--- src/main/java/com/wentuotuo/algolia/model/Hexo.java
+++ src/main/java/com/wentuotuo/algolia/model/Hexo.java
@@ -5,11 +5,21 @@ package com.wentuotuo.algolia.model;
  * AlgoliaSearch
  */
 public class Hexo {
+    private String objectID;
     private String title;
     private String date;
     private String slug;
     private String path;
     private String content;
+
+    public void print(){
+        //System.out.println("objectID: " + objectID);
+        System.out.println("title: " + title);
+        System.out.println("date: " + date);
+        System.out.println("slug: " + slug);
+        System.out.println("path: " + path);
+        //System.out.println("content: " + content);
+    }
 
     public String getTitle() {
         return title;
@@ -17,6 +27,14 @@ public class Hexo {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getObjectID() {
+        return objectID;
+    }
+
+    public void setObjectID(String objectID) {
+        this.objectID = objectID;
     }
 
     public String getDate() {
--- src/main/java/com/wentuouto/algolia/app/Algolia.java
+++ src/main/java/com/wentuouto/algolia/app/Algolia.java
@@ -1,20 +1,90 @@
 package com.wentuouto.algolia.app;
 
+import com.algolia.search.APIClient;
+import com.algolia.search.APIClientBuilder;
+import com.algolia.search.ApacheAPIClientBuilder;
 import com.algolia.search.Index;
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.objects.Query;
 import com.algolia.search.responses.SearchResult;
-import com.wentuotuo.algolia.AlgoliaManager;
 import com.wentuotuo.algolia.model.Hexo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Ekimin on 2017/3/1.
- * AlgoliaSearch
+ * https://www.algolia.com/doc/api-client/java/getting-started/
  */
 public class Algolia {
 
-    public static  void main(String[] args){
-        AlgoliaManager algoliaManager = new AlgoliaManager("0XHJNLJKH5", "a8aa45462154736510bedf5ddadc838e");
+    public static void main(String[] args) throws AlgoliaException {
 
-        Index<Hexo> index = algoliaManager.apiClient.initIndex("Hexo", Hexo.class);
-        
+        APIClient client = new ApacheAPIClientBuilder("0XHJNLJKH5", "df8136b265109307bfbbe5576287d635").build();
+        Index<Hexo> index = client.initIndex("Hexo", Hexo.class);
+        Query query = new Query("*").setAttributesToRetrieve(Arrays.asList("title", "date", "path", "content")).setHitsPerPage(50);
+        SearchResult<Hexo> sr = index.search(query);
+        List<Hexo> hexoList = sr.getHits();
+
+        String url = "jdbc:mysql://47.90.56.252:3306/test?user=root&password=mysql123456&amp;useUnicode=true&characterEncoding=UTF-8";
+        String sql = "INSERT INTO Hexo (ID, title, date, path, content) values (?,?,?,?,?)";
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = DriverManager.getConnection(url);
+            ps = con.prepareStatement(sql);
+
+            int count = 0;
+            for (Hexo hexo : hexoList) {
+                String ID = UUID.randomUUID().toString();
+                ps.setString(1, ID);
+                ps.setString(2, hexo.getTitle());
+                ps.setString(3, hexo.getDate());
+                ps.setString(4, hexo.getPath());
+                ps.setString(5, hexo.getContent());
+
+                System.out.println(hexo.getTitle());
+                ps.addBatch();
+                count++;
+                if (count >= 500) {
+                    ps.executeBatch();
+                    count = 0;
+                }
+            }
+            if (count > 0) {
+                ps.executeBatch();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        System.out.println();
     }
 }
